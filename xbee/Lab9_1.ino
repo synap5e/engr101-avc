b6c9ef7ae31d40cb79d3c121c1a0d3ebabd53b64
synap53 <code@gilbertpinfold.co.nz>
2013-05-19 12:56:21 +1200

    Added example code

---
+++ xbee/Lab9_1.ino
@@ -0,0 +1,31 @@
+// code for receiving data from XBee radio
+// xBee is connected to pins 4 and 5
+
+
+#include <SoftwareSerial.h>
+
+#define zb_txPin 5
+#define zb_rxPin 4
+
+SoftwareSerial xBeeSerial(zb_rxPin, zb_txPin); // RX, TX
+
+void setup()
+{
+  Serial.begin(9600);
+  xBeeSerial.begin(9600);
+}
+
+
+void loop()
+{
+  byte inByte;
+  while ( xBeeSerial.available() > 0)
+  {
+    inByte = xBeeSerial.read();
+    Serial.print("rcvd: ");
+    Serial.println(inByte,HEX);
+  }
+  
+}
+
+
