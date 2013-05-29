d836b64b3a20c2cf1ad5dfdba48f5aa53f041824
synap53 <code@gilbertpinfold.co.nz>
2013-05-29 20:47:54 +1200

    Completed joystick test case

--- xbee/joystick-test.py
+++ xbee/joystick-test.py
@@ -1,23 +1,38 @@
-import pygame, copy, time
-from pygame.locals import *
+import pygame
 
 pygame.init()
+ 
+# Set the width and height of the screen [width,height]
+size = [500, 700]
+screen = pygame.display.set_mode(size)
 
-pygame.joystick.init()
-if not pygame.joystick.get_count():
-   print "nPlease connect a joystick and run again.n"
-   quit()
+pygame.display.set_caption("My Game")
 
-screen = pygame.display.set_mode((640, 480))
-pygame.display.set_caption('AVC')
-
+#Loop until the user clicks the close button.
 done = False
-joystick_count = pygame.joystick.get_count()
-while not done:
-	for stick in range(joystick_count):
-		joystick = pygame.joystick.Joystick(stick)
-		axes = joystick.get_numaxes()
-		for axis in range( axes ):
-			value = joystick.get_axis( axis )
-			print "Joystick " + stick + ", axis " + axis + "\t\t" + value
 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Initialize the joysticks
+pygame.joystick.init()
+   
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+   
+while done==False:
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+
+	joystick_count = pygame.joystick.get_count()
+
+	print "%s\t%s" % (round(joystick.get_axis(1), 3), round(joystick.get_axis(2), 3))
+
+    # Limit to 10 updates per second
+    clock.tick(10)
+    
+# Close the window and quit.
+# If you forget this line, the program will 'hang'
+# on exit if running from IDLE.
+pygame.quit ()
